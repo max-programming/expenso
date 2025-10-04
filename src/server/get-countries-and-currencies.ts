875827031350b3160b85acaa184f289297e5eeca
Usman S. <ui.sabuwala@gmail.com>
2025-10-04 04:39:38 +0000

    countries and currencies data fetching

---
+++ src/server/get-countries-and-currencies.ts
@@ -0,0 +1,31 @@
+import { createServerFn } from "@tanstack/react-start";
+import { staticFunctionMiddleware } from "@tanstack/start-static-server-functions";
+
+export const getCountriesAndCurrencies = createServerFn({ method: "GET" })
+  .middleware([staticFunctionMiddleware])
+  .handler(async () => {
+    const res = await fetch(
+      "https://restcountries.com/v3.1/all?fields=name,currencies"
+    );
+    const data = await res.json();
+    return data as Country[];
+  });
+
+export interface Country {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+}
